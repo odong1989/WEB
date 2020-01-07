a65355c6921a449b5674cd04eb4ec5a30b75c468
kwunodong <kwunodong@gmail.com>
2020-01-07 21:28:37 +0900

    DATE 200107 UDEMY javaScript Study

---
+++ JavaScript/UDEMY_Modern JavaScript From The Beginning/ch2.2_using to console/app.js
@@ -0,0 +1,28 @@
+//Log to Console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+
+var greeting = 'hello by greeting';
+
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('this is some error');
+console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+  console.log('Hello world');
+  console.log('Hello world');
+  console.log('Hello world');
+  console.log('Hello world');
+  console.log('Hello world');
+  console.log('Hello world');
+ console.timeEnd('Hello'); 
+/*
+    multi
+    line
+    comments
+*/ 
---
+++ JavaScript/UDEMY_Modern JavaScript From The Beginning/ch2.3_Variables - var, let & const/app.js
@@ -0,0 +1,9 @@
+// var, let, const
+
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+var greeting;
+console.log(greeting);
