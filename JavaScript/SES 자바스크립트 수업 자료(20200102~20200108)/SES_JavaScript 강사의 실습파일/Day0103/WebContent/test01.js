91ecdb1ea6e315e337cce9fcdf794d493615ebdf
kwunodong <kwunodong@gmail.com>
2020-01-08 11:49:01 +0900

    DATE 200108 SES JavaScript 수업 5일차(마지막)

---
+++ JavaScript/SES 자바스크립트 수업 자료(20200102~20200108)/SES_JavaScript 강사의 실습파일/Day0103/WebContent/test01.js
@@ -0,0 +1,41 @@
+// alert(1 == 0);
+
+console.log(1 == "1");
+console.log("" == false);
+console.log(undefined == null);
+
+console.log(1 === "1");
+console.log("" === false);
+console.log(undefined === null);
+console.log("\n");
+
+console.log(parseInt(3));
+console.log(parseInt(3.14));
+console.log(parseInt("3"));
+console.log(parseInt(true));
+console.log(parseInt(undefined));
+console.log(parseInt(null));
+
+console.log(parseFloat(3));
+console.log("\n");
+
+console.log(new Number(3));
+console.log(new Number(3.14));
+console.log(new Number("3"));
+console.log(new Number("3.14"));
+console.log(new Number(false));
+console.log(new Number(undefined));
+console.log(new Number(null));
+
+console.log("3.14" * 1);
+
+
+
+
+
+
+
+
+
+
+
