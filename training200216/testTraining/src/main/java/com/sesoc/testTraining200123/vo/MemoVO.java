1c548286337abdac6af135a1bcdeb9fe3a36493e
odong1989 <kwunodong@gmail.com>
2020-02-16 23:21:20 +0900

    Date 200216_Date200123_처음부터다시하여insert까지_의 추가진행(select 진행중)

---
+++ training200216/testTraining/src/main/java/com/sesoc/testTraining200123/vo/MemoVO.java
@@ -0,0 +1,13 @@
+package com.sesoc.testTraining200123.vo;
+
+import lombok.Data;
+
+@Data
+public class MemoVO {
+	
+	private int		 memo_no;
+	private String   memo_pw; 
+	private String	 memo_content;
+	private String 	 memo_indate;
+
+}
